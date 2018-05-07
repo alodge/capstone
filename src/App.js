196ac2270b9b23174cce83c771ad5ec5054525f7
alodge <lodgea@oregonstate.edu>
2018-05-06 17:55:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,8 +40,7 @@ class App extends Component {
     console.log(emailDomain);
     if (emailDomain == "oregonstate.edu") {
       fetch('https://my-project-1514223225812.appspot.com/account', {
-        method: 'get',
-        body: JSON.stringify({address: emailAddress})
+        method: 'get'
         }).then(res => console.log(res));
       const myAddress = await web3.eth.getAccounts();
       this.setState({ message: "Waiting on transaction success.." });
