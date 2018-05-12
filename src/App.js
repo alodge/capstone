271e0c7f3e14c4d9738f2231a759d5b5028ad583
alodge <lodgea@oregonstate.edu>
2018-05-12 11:11:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ class App extends Component {
             Email Address:
             <input type="text" value={this.state.emailAddy} onChange={this.updateState} />
           </label>
-          <hr />
+          <br>
           <button>Get Tokens!</button>
           <hr />
           <h1>{this.state.message}</h1>
